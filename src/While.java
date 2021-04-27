68d4948ee03d97196256295b41988b0912cdb0fa
Evgeny <johons@inbox.ru>
2021-04-27 09:36:41 +0300

    While added

---
+++ src/While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String[] args) {
+        int i = 200000;
+        while (i <= 350000){
+            System.out.println("Номер билета: " + i);
+            if (i == 210000) {
+                i = 345000;
+                System.out.println("===================");
+            } else i = i + 100;
+        }
+    }
+}
