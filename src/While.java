b1070617eda818545930c5ff42e2ce6e7994a98c
Evgeny <johons@inbox.ru>
2021-04-28 12:32:43 +0300

    FixWhile

--- src/While.java
+++ src/While.java
@@ -1,6 +1,7 @@
 public class While {
     public static void main(String[] args) {
         int i = 200000;
+        System.out.println("Программа вывода номеров билетов...");
         while (i <= 350000){
             System.out.println("Номер билета: " + i);
             if (i == 205000) {
